d4339818d7396882d354050b35f2a31068408981
John Lee <drumwolf@Johns-MacBook-Pro.local>
2022-01-02 10:37:55 -0800

    create function to format raw API data and set state for calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,12 +1,35 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { fetchAPI } from '../services/gsx'
+
+type itineraryType = {
+  [key: string]: string[]
+}
+
+const formatGSXData = (data: string[][]): itineraryType => {
+  const dataObj: itineraryType = {}
+  let key = ''
+  data.forEach(entry => {
+    const [k, v] = entry
+    key = (k !== '') ? k : key
+    if (!dataObj[key]) {
+      Object.assign(dataObj, { [key]: [v] })
+    } else {
+      dataObj[key].push(v)
+    }
+  })
+  return dataObj
+}
 
 const Calendar = () => {
 
+  const [gsxData, setGSXData] = useState<itineraryType>({})
+
+  console.log(gsxData)
   useEffect(() => {
     async function getAPI() {
-      const data = await fetchAPI()
-      console.log(data)
+      const rawData = await fetchAPI()
+      const formattedData = formatGSXData(rawData)
+      setGSXData(formattedData)
     }
     getAPI()
   }, [])
