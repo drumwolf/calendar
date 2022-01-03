31c4d14f3698b3c049981f48ca6bda7289e426a9
John Lee <drumwolf@Johns-MacBook-Pro.local>
2022-01-02 19:27:45 -0800

    print dates

--- src/components/CalendarGrid.tsx
+++ src/components/CalendarGrid.tsx
@@ -2,10 +2,15 @@ import { getNextSixWeeks } from '../helpers/date'
 
 type CalendarGridProps = { startDate: Date }
 
+const printDate = (date: Date): string => date.toISOString().split('T')[0]
+
 const CalendarGrid: React.FC<CalendarGridProps> = ({ startDate }) => {
   const dates = getNextSixWeeks(startDate)
-  console.log('DATEZ::::', dates)
-  return <ul></ul>
+  return (<ul>
+    {
+      dates.map(date => <li>{printDate(date)}</li>)
+    }
+  </ul>)
 }
 
 export default CalendarGrid
