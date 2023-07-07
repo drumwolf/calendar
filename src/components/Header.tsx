81df1e46a073c7998816f1c3bc5e2a4b50c09d0f
drumwolf <drumwolf@Jakes-MBP.localdomain>
2023-07-07 12:21:20 -0700

    added toggle buttons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,43 @@
-import { Paper, Typography } from '@mui/material'
+import { Button, Paper, Typography } from '@mui/material'
+import DoubleArrowOutlinedIcon from '@mui/icons-material/DoubleArrowOutlined'
+import { StartDateContext } from '../contexts/StartDateContext'
+import { useContext } from 'react'
 
-const Header = () => <Paper
-  elevation={3}
-  sx={{
-    bgcolor: '#e7e7e7',
-    padding: '10px',
-    height: '5vh',
-    justifyContent: 'center',
-    display: 'flex',
-    alignItems: 'center'
-  }}
->
-  <Typography variant="h4">
-    January 2022
-  </Typography>
-</Paper>
+const lastWeekDate = (date: Date) => new Date(date.getFullYear(), date.getMonth(), date.getDate() - 7)
+const nextWeekDate = (date: Date) => new Date(date.getFullYear(), date.getMonth(), date.getDate() + 7)
+
+const Header = () => {
+  const { startDate, setStartDate } = useContext(StartDateContext)
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        bgcolor: '#e7e7e7',
+        padding: '10px',
+        height: '5vh',
+        justifyContent: 'space-between',
+        display: 'flex',
+        alignItems: 'center'
+      }}
+    >
+      <Button
+        variant="contained"
+        onClick={() => setStartDate(lastWeekDate(startDate))}
+        sx={{ transform: 'scaleX(-1)' }}
+      >
+        <DoubleArrowOutlinedIcon />
+      </Button>
+      <Typography variant="h4">
+        January 2022
+      </Typography>
+      <Button
+        variant="contained"
+        onClick={() => setStartDate(nextWeekDate(startDate))}
+      >
+        <DoubleArrowOutlinedIcon />
+      </Button>
+    </Paper>
+  )
+}
 
 export default Header
